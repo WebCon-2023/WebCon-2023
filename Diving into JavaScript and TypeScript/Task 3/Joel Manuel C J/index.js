3a7cbac816b984a2220b93e60b22f0e7149adce6
Joel Manuel C J <joelmanuelcj@gmail.com>
2023-04-23 22:55:17 +0530

    initial commit

---
+++ Diving into JavaScript and TypeScript/Task 3/Joel Manuel C J/index.js
@@ -0,0 +1,30 @@
+
+document.getElementById("loginid").defaultValue = "Enter phone number or email";
+var x=1,id="login";
+function check()
+{
+    if(document.getElementById("loginid").value=="")
+    {
+        id=document.getElementById("loginid").value
+        var loginid = document.getElementById("err1")
+        var element="<h3>Please enter a phone number or email</h3>"
+        loginid.innerHTML += element
+    }
+    else{
+        console.log(id)
+        next_page()
+    }
+}
+function otp_generation()
+{
+    x = Math.floor((Math.random() * 10000) + 1);
+    alert("Your OTP is"+x);
+}
+function next_page() {
+    location.replace("otp.html")
+    otp_generation()
+  }
+function otp_position()
+{
+    console.log(x+"  "+id)
+}
